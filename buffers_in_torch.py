1d280d0d42b53ce465ab87c4627e0465c719795e
Jan <jankrepl@yahoo.com>
2022-08-31 15:32:30 +0200

    Add torch examples

---
+++ buffers_in_torch.py
@@ -0,0 +1,9 @@
+import torch
+bn = torch.nn.BatchNorm1d(5)
+bn.state_dict()
+
+for name, p in bn.named_buffers():
+    print(name, p, p.requires_grad)
+
+for name, p in bn.named_parameters():
+    print(name, p, p.requires_grad)
